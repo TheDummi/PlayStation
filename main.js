f76041d3c404ed2a2d93721fc2347ff302a38f6b
Dummi <raydecrom@hotmail.com>
2023-05-19 19:56:12 +0200

    fixed styling and navigation

--- main.js
+++ main.js
@@ -4,19 +4,20 @@ const main = document.getElementsByTagName('main').item(0);
 
 if (header) {
 	header.innerHTML = `<div class="container">
-    <a class="header-link" href="/PSX">PlayStation X</a>
-    <a class="header-link" href="/PS2">PlayStation 2</a>
-    <a class="header-link" href="/PS3">PlayStation 3</a>
-    <a class="header-link" href="/PS4">PlayStation 4</a>
-    <a class="header-link" href="/PSP">PlayStation Portable</a>
-    <a class="header-link" href="/PSVita">PlayStation Vita</a>
+	<a class="header-link" href="/">Home</a>
+    <a class="header-link" href="/PlayStation/PSX">PlayStation X</a>
+    <a class="header-link" href="/PlayStation/PS2">PlayStation 2</a>
+    <a class="header-link" href="/PlayStation/PS3">PlayStation 3</a>
+    <a class="header-link" href="/PlayStation/PS4">PlayStation 4</a>
+    <a class="header-link" href="/PlayStation/PSP">PlayStation Portable</a>
+    <a class="header-link" href="/PlayStation/PSVita">PlayStation Vita</a>
     <a class="header-link" href="https://discord.gg/tWFDYBj9ZC" target="_blank">Join our server!</a>
     </div>`;
 }
 
 if (aside && main) {
 	if (main.children.length <= 0) {
-		main.innerHTML = '<center><h1>There is no articles here yet.</h1></center>';
+		main.innerHTML = '<center><h1>There are no articles here yet.</h1></center>';
 
 		aside.remove();
 	}
